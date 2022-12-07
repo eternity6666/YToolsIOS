664cc0c5300238c6866061f2b5d3189dd6e0e0ce
eternity6666 <eternity6666@qq.com>
2022-12-07 17:22:34 +0800

    Initial Commit

---
+++ YTools/YToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YToolsApp.swift
+//  YTools
+//
+//  Created by eternity6666 on 2022/12/7.
+//
+
+import SwiftUI
+
+@main
+struct YToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
